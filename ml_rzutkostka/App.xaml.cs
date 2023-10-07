6a49c3fd1d14bd83e07aa781a774bcddb25db317
Guru18650 <guruwszystkiego@gmail.com>
2023-10-07 21:49:40 +0200

    Dodaj pliki projektów.

---
+++ ml_rzutkostka/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace ml_rzutkostka
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ ml_rzutkostka/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System.Threading;
+
+namespace ml_rzutkostka
+{
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            int i = rnd.Next(1, 6);
+            animatedImage.Source = $"d{i}.gif";
+            animatedImage.IsAnimationPlaying = true;
+            await Task.Delay(6000);
+            animatedImage.IsAnimationPlaying = false;
+        }
+    }
+}
